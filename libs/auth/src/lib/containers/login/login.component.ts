8dd4002a9cbf7155a40d163a99fa50c8ab140ec9
DenysWarlan <denys.varlan95@gmail.com>
2022-12-20 19:08:28 +0100

    add libs auth

---
+++ libs/auth/src/lib/containers/login/login.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngxs/store';
+import { filter, Observable } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { takeUntil } from 'rxjs/operators';
+import { AuthState } from '../../reducers/auth.state';
+import { Login } from '../../reducers/actions/login.actions';
+import { SetToken } from '../../reducers/actions/setToken.actions';
+import { Destroy } from '@monorepo/destroy';
+
+@Component({
+  selector: 'monorepo-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent extends Destroy implements OnInit {
+  public isAuthSuccess$: Observable<boolean> = this.store.select(
+    AuthState.isAuthSuccess
+  );
+  public isAuthLoading$: Observable<boolean> = this.store.select(
+    AuthState.isAuthSuccess
+  );
+  public authErrors: Observable<HttpErrorResponse | null> = this.store.select(
+    AuthState.authErrors
+  );
+  public form!: FormGroup;
+  private charsCount: number = 6;
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private store: Store
+  ) {
+    super();
+  }
+  public ngOnInit(): void {
+    this.form = this.createForm();
+
+    this.handleIsAuthSuccess();
+  }
+
+  public loginUser(): void {
+    const params: any = this.form.value;
+    this.store.dispatch(new Login({ data: params }));
+  }
+
+  private createForm(): FormGroup {
+    return new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(this.charsCount),
+      ]),
+    });
+  }
+
+  private handleIsAuthSuccess(): void {
+    this.isAuthSuccess$
+      .pipe(filter(Boolean), takeUntil(this.subGuard$))
+      .subscribe(() => {
+        this.store.dispatch(
+          new SetToken({ data: localStorage.getItem('token') })
+        );
+        this.router.navigate(['home']);
+      });
+  }
+}
---
+++ libs/auth/src/lib/containers/register/register.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Store } from '@ngxs/store';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Register } from '../../reducers/actions/register.actions';
+import { AuthState } from '../../reducers/auth.state';
+import { filter, Observable, tap } from 'rxjs';
+
+@Component({
+  selector: 'monorepo-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss'],
+})
+export class RegisterComponent implements OnInit {
+  public form!: FormGroup;
+  private charsCount: number = 6;
+  private isRegister: Observable<boolean> = this.store.select(
+    AuthState.isRegister
+  );
+  constructor(
+    private store: Store,
+    private router: Router,
+    private route: ActivatedRoute,
+    private fb: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    this.form = this.createForm();
+    this.handleRegister();
+  }
+
+  private createForm(): FormGroup {
+    return this.fb.group({
+      email: this.fb.control('', [Validators.required, Validators.email]),
+      password: this.fb.control('', [
+        Validators.required,
+        Validators.minLength(this.charsCount),
+      ]),
+    });
+  }
+
+  public submitForm(): void {
+    const params: any = this.form.value;
+    this.store.dispatch(new Register({ data: params }));
+  }
+
+  public login(): void  {
+    this.router.navigate(['/auth/login'], {
+      relativeTo: this.route,
+    });
+  }
+
+  public handleRegister(): void  {
+    this.isRegister.pipe(
+      filter(Boolean),
+      tap(() => this.login())
+    ).subscribe();
+  }
+}
